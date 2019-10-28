b80b612052eac3754b35a22cfded8262817bb2a0
SkiingxMoose <JoshDesmond@users.noreply.github.com>
2019-10-28 19:32:47 -0400

    Added ideavim support to powershell script

--- neovim/SetupNeovimProfile.ps1
+++ neovim/SetupNeovimProfile.ps1
@@ -1,3 +1,9 @@
+$isWindows = ($env:OS -like "*windows*")
+if (! $isWindows) {
+	write-error "OS doesn't seem to be Windows"
+	Break Script
+}
+
 $appdatalocalpath = "$ENV:UserProfile\AppData\Local\"
 if (!(test-path $appdatalocalpath)) {
 	write-error "Can't find path $appdatalocalpath"
@@ -17,6 +23,17 @@ if(test-path ".\init.vim") {
 	cat .\init.vim
 } else {
 	"source $PSScriptRoot\_vimrc" > ".\init.vim"
+}
+
+# The following block is for making your _vimrc work with ideavim
+{
+	cd "$ENV:UserProfile\"
+	if (test-path ".\.ideavimrc") {
+		write-host "Error: ~\ideavimrc already exists. Printing contents and exiting script"
+		cat .\init.vim
+	} else {
+		"source $PSScriptRoot\_vimrc" > ".\.ideavimrc"
+	}
 }
 
 cd $PSScriptRoot
