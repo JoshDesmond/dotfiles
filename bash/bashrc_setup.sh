bd3c82d923eca8537769265133e74792d621dd8e
JoshDesmond <JoshDesmond@users.noreply.github.com>
2020-05-11 19:35:30 -0400

    Tweaks/error checking for bashrc and setup

--- bash/bashrc_setup.sh
+++ bash/bashrc_setup.sh
@@ -2,8 +2,20 @@
 # Sets up .bashrc
 
 # TODO grep ~/.bashrc and check for "/.bash_personal" so you can know if it's already a thing
+SCRIPT_GREP_RESULT=$(grep ".bash_personal_aliases" ~/.bashrc )
 
-printf "\n\n" >> ~/.bashrc
-printf "source $PWD/.bash_personal_aliases\n" >> ~/.bashrc
-printf "source $PWD/.bash_personal_config\n\n" >> ~/.bashrc
+if [ -n "$SCRIPT_GREP_RESULT" ] ; then
+	echo ".bashrc already seems to be configured. Outputing .bashrc tail:"
+	echo "======== ~/.bashrc ========"
+	tail ~/.bashrc
+	echo "==========================="
+	printf "\nExiting script\n"
+	exit 0
+fi
+
+
+
+# printf "\n\n" >> ~/.bashrc
+# printf "source $PWD/.bash_personal_aliases\n" >> ~/.bashrc
+# printf "source $PWD/.bash_personal_config\n\n" >> ~/.bashrc
 
