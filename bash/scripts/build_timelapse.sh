eb15eb28cc59eb19cf64685e4260bb7f90ce1405
JoshDesmond <JoshDesmond@users.noreply.github.com>
2020-05-11 19:36:09 -0400

    Some quick bash script updates

--- bash/scripts/build_timelapse.sh
+++ bash/scripts/build_timelapse.sh
@@ -19,6 +19,8 @@ sort files.txt >/dev/null
 
 echo "sorting finished"
 
-mencoder -ovc x264 -mf w=1920:h=1080:fps=60:type=jpg 'mf://@files.txt' -o "$(date +%F%H)-screenlapse.avi"
+# mencoder -ovc x264 -mf w=1920:h=1080:fps=60:type=jpg 'mf://@files.txt' -o
+# "$(date +%F%H)-screenlapse.avi" # this was the original command
+mencoder -noskip -ovc x264 -x264encopts subq=7:frameref=4:threads=auto:qcomp=0.9 -mf w=1920:h=1080:fps=60:type=jpg 'mf://@files.txt' -o "$(date +%F%H)-screenlapse.avi"
 
 popd
