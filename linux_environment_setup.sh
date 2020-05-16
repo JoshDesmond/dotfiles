88585bb9eccea3d1239ab1df2e1a30b0bb55ae5b
JoshDesmond <JoshDesmond@users.noreply.github.com>
2020-05-16 13:33:50 -0400

    Linux environment setup tweaks/Fixes

--- linux_environment_setup.sh
+++ linux_environment_setup.sh
@@ -24,6 +24,7 @@ sudo apt-get --assume-yes install git
 # Do some git setup
 git config --global user.email "JoshDesmond@users.noreply.github.com"
 git config --global user.name "JoshDesmond"
+git remote set-url origin "git@github.com:JoshDesmond/dotfiles.git"
 
 # Setup ~/code/ folders
 cd ~
