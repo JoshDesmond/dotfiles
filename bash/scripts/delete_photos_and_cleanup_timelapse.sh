671aafb5731a3414cd781976ba1a6fc320d27d38
JoshDesmond <JoshDesmond@users.noreply.github.com>
2020-05-19 11:44:06 -0400

    First commit on automating timelapse cleanup

--- bash/scripts/delete_photos_and_cleanup_timelapse.sh
+++ bash/scripts/delete_photos_and_cleanup_timelapse.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-# TODO
-# check for and create a "mutex" lock to indicate this script is running.
+path_timelapse_dir="$HOME/Pictures/timelapse"
+path_mutex="$path_timelapse_dir/mutex"
+
+if [ -f "$path_mutex" ]; then
+	echo "file $path_mutex found, printing\
+	contents and exiting"
+	cat $path_mutex
+	exit 1
+fi
+
+printf "Starting" > "$path_mutex"
+pushd $path_timelapse_dir
+
+path_files="$path_timelapse_dir/files.txt"
+if ! [ -f "$path_files" ]; then
+	echo "$path_files not found, hmmm..."
+	exit 1
+fi
+
+
+
 # Make a backup of files.txt, use the file name of the latest video that was created (gzip the file)
 # Copy the video file that was created to the media drive
 # Delete all of the files that were listed in files.txt
@@ -12,3 +31,6 @@
 # Bonus: Print the length of the video, the number of photos that are being deleted, and the first and last
 # timestamps of photos and add a confirmation dialogue to the user before executing any data deletion
 
+rm $path_mutex
+
+popd
--- bash/scripts/screenlapser.sh
+++ bash/scripts/screenlapser.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # First create and navigate to a new folder
-cd ~/Pictures/timelapse
+cd ~/Pictures/timelapse # TODO refactor with other photo scripts
 var_time=$(date +%Y-%m-%d-%H%M%S)
 mkdir $var_time
 cd "./$var_time"
 
 # Now take pictures every 1 second
-while [ 1 ]; do scrot --silent -q 100 $(date +%Y-%m-%d%H%M%S).jpg; sleep 2; done
+while [ 1 ]; do scrot --silent -q 100 $(date +%Y-%m-%d%H%M%S).jpg; sleep 4; done
 
 # TODO move files to the media drive or data drive, perform encoding.
