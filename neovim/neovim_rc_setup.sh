b87896c405dd2396331b7c90c95d774db3987116
JoshDesmond <JoshDesmond@users.noreply.github.com>
2022-06-14 17:52:40 -0400

    Adds vim-plug install to vim in bash setup

--- neovim/neovim_rc_setup.sh
+++ neovim/neovim_rc_setup.sh
@@ -37,4 +37,8 @@ echo "source $PWD/_vimrc" >> ~/.config/nvim/init.vim
 echo "source $PWD/_vimrc" >> ~/.vimrc
 echo "source $PWD/_vimrc" >> ~/.ideavimrc
 
+# Install vim-plug: See https://github.com/junegunn/vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 popd
