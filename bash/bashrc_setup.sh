7440fe1f5bfa4706394084d40f660520de3e53ff
JoshDesmond <JoshDesmond@users.noreply.github.com>
2022-07-08 14:33:55 -0400

    Add setup configuration for inputrc customization

--- bash/bashrc_setup.sh
+++ bash/bashrc_setup.sh
@@ -22,5 +22,18 @@ printf "\n\n" >> ~/.bashrc
 printf "source $PWD/.bash_personal_aliases\n" >> ~/.bashrc
 printf "source $PWD/.bash_personal_config\n\n" >> ~/.bashrc
 
+# Set up case-insensitivity (from: https://askubuntu.com/q/87061/1039153):
+# If ~/.inputrc doesn't exist yet: First include the original /etc/inputrc
+# so it won't get overriden
+if [ ! -a ~/.inputrc ]; then echo '$include /etc/inputrc' > ~/.inputrc; fi
+# Add shell-option to ~/.inputrc to enable case-insensitive tab completion
+
+# Set up auto-complete cycle (from: https://unix.stackexchange.com/a/447638/403042)
+echo 'set completion-ignore-case On' >> ~/.inputrc
+echo 'set show-all-if-ambiguous on' >> ~/.inputrc
+echo 'set show-all-if-unmodified on' >> ~/.inputrc
+echo 'set menu-complete-display-prefix on' >> ~/.inputrc
+echo '"\t": menu-complete' >> ~/.inputrc
+
 source ~/.bashrc
 
