54f8cbaea2bc52907fa7f32cdb6c55ab651f6c22
JoshDesmond <JoshDesmond@users.noreply.github.com>
2023-08-09 22:49:44 -0400

    Updates bashrc_setup for macOs

--- bash/bashrc_setup.sh
+++ bash/bashrc_setup.sh
@@ -1,32 +1,45 @@
 #!/bin/bash
-# Sets up .bashrc
+# Sets up .bashrc or .bash_profile on macOS
 
 # cd to the directory this script is in
 cd "${0%/*}"
 
-# Check if .bashrc is already set up
-SCRIPT_GREP_RESULT=$(grep ".bash_personal_aliases" ~/.bashrc )
+# Check the OS
+OS="$(uname)"
 
-if [ -n "$SCRIPT_GREP_RESULT" ] ; then
-	echo ".bashrc already seems to be configured. Outputing .bashrc tail:"
-	echo "======== ~/.bashrc ========"
-	tail ~/.bashrc
+# Check if .bashrc or .bash_profile (for macOS) is already set up
+if [ "$OS" == "Darwin" ]; then
+  BASHRC_PATH="$HOME/.bash_profile"
+else
+  BASHRC_PATH="$HOME/.bashrc"
+fi
+
+SCRIPT_GREP_RESULT=$(grep ".bash_personal_aliases" "$BASHRC_PATH")
+
+if [ -n "$SCRIPT_GREP_RESULT" ]; then
+	echo "$BASHRC_PATH already seems to be configured. Outputting tail:"
+	echo "======== $BASHRC_PATH ========"
+	tail "$BASHRC_PATH"
 	echo "==========================="
 	printf "\nExiting script\n"
 	exit 0
 fi
 
-printf "\n\n" >> ~/.bashrc
-printf "source $PWD/.bash_personal_aliases\n" >> ~/.bashrc
-printf "source $PWD/.bash_personal_config\n\n" >> ~/.bashrc
+printf "\n\n" >> "$BASHRC_PATH"
+printf "source $PWD/.bash_personal_aliases\n" >> "$BASHRC_PATH"
+printf "source $PWD/.bash_personal_config\n\n" >> "$BASHRC_PATH"
 export PATH="$PATH:$HOME/code/dotfiles/bash/scripts"
 
 # Set up case-insensitivity (from: https://askubuntu.com/q/87061/1039153):
 # If ~/.inputrc doesn't exist yet: First include the original /etc/inputrc
-# so it won't get overriden
-if [ ! -a ~/.inputrc ]; then echo '$include /etc/inputrc' > ~/.inputrc; fi
-# Add shell-option to ~/.inputrc to enable case-insensitive tab completion
+# so it won't get overridden
+if [ ! -a ~/.inputrc ]; then
+  if [ -f /etc/inputrc ]; then
+    echo '$include /etc/inputrc' > ~/.inputrc
+  fi
+fi
 
+# Add shell-option to ~/.inputrc to enable case-insensitive tab completion
 # Set up auto-complete cycle (from: https://unix.stackexchange.com/a/447638/403042)
 echo 'set completion-ignore-case On' >> ~/.inputrc
 echo 'set show-all-if-ambiguous on' >> ~/.inputrc
@@ -34,5 +47,5 @@ echo 'set show-all-if-unmodified on' >> ~/.inputrc
 echo 'set menu-complete-display-prefix on' >> ~/.inputrc
 echo '"\t": menu-complete' >> ~/.inputrc
 
-source ~/.bashrc
+source "$BASHRC_PATH"
 
