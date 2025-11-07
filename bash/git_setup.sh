cb0b172cebbc36c2b031a537ee63f811758d419d
JoshDesmond <JoshDesmond@users.noreply.github.com>
2025-11-07 14:55:51 -0500

    Updates SSH setup in git config script

--- bash/git_setup.sh
+++ bash/git_setup.sh
@@ -9,7 +9,14 @@ git remote set-url origin "git@github.com:JoshDesmond/dotfiles.git"
 git config --global core.editor nvim
 git config --global push.autoSetupRemote true
 git config --global gpg.format ssh
-git config --global user.signingkey ~/.ssh/id_rsa.pub # TODO, update this to match ssh_setup.sh
 git config --global commit.gpgsign true
 git config --global init.defaultBranch main
 git config --global pull.rebase true
+
+# SSH Key setup
+KEY_PUB="$HOME/.ssh/id_ed25519.pub" # TODO, update this to match ssh_setup.sh
+if [[ -f "$KEY_PUB" ]]; then
+    git config --global user.signingkey "$KEY_PUB"
+else
+    echo "Warning: SSH public key not found at $KEY_PUB, skipping signing key setup"
+fi
