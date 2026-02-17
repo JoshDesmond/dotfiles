ade44878a2bdb903db9f3f9b88d805c42cf23643
Josh Desmond <JoshDesmond@users.noreply.github.com>
2026-02-17 17:28:26 -0500

    Adds npmrc configuration scripts

--- node/node_setup.sh
+++ node/node_setup.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 npm config set init-author-name "J. Desmond"
 npm config set fund false
 
-# Security hardening
+# Security
 npm config set ignore-scripts true
 npm config set audit-level moderate
 
